f85bbfa58a647a09bcc34a6deff4feb12999c7f3
kozhii <alexkozhokin@Alexs-MacBook-Pro-2.local>
2023-08-09 23:57:26 -0400

    Initial commit

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "6ab3291287578e743fff9471730fd2a1"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "e4efe5203c5948009383e7ed91a14aff"
